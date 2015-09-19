dbccf03120e8e202e807fe19346c67fc061d27f3
Franck Arnulfo <franck.arnulfo@gmail.com>
2015-09-19 18:32:55 +0200

    toString

--- src/main/java/org/pipoware/pst/exp/BID.java
+++ src/main/java/org/pipoware/pst/exp/BID.java
@@ -28,4 +28,25 @@ public class BID {
     return (bid & 0b10) != 0 ;
   }
   
+  public static String toString(long bid) {
+    return "0x" + Long.toHexString(bid) + " " + ((isInternal(bid) ? "INTERNAL" : "NOT INTERNAL"));
+  }
+
+  public static String toString(long[] arrayOfBID) {
+        if (arrayOfBID == null)
+            return "null";
+        int iMax = arrayOfBID.length - 1;
+        if (iMax == -1)
+            return "[]";
+
+        StringBuilder b = new StringBuilder();
+        b.append('[');
+        for (int i = 0; ; i++) {
+            b.append(BID.toString(arrayOfBID[i]));
+            if (i == iMax)
+                return b.append(']').toString();
+            b.append(", ");
+        }
+    }
+    
 }
