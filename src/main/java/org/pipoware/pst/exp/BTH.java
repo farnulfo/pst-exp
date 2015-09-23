6c9f2131c897e4d4da38d8a8f5f36b73a1bfc31e
Franck Arnulfo <franck.arnulfo@gmail.com>
2015-09-24 00:51:27 +0200

    Add BTH class

---
+++ src/main/java/org/pipoware/pst/exp/BTH.java
@@ -0,0 +1,30 @@
+package org.pipoware.pst.exp;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Preconditions;
+
+/**
+ *
+ * @author Franck Arnulfo
+ */
+public class BTH {
+  
+  private final HN hn;
+  
+  public BTH(HN aHN) {
+    Preconditions.checkArgument(
+      aHN.bClientSig == HN.CLIENT_SIG_PC_BTH,
+      "Unsupported bClientSig 0x%s", Integer.toHexString(Byte.toUnsignedInt(aHN.bClientSig)));
+    this.hn = aHN;
+    
+    HID hid = new HID(hn.hidUserRoot);
+
+  }
+
+  @Override
+  public String toString() {
+    return MoreObjects.toStringHelper(this)
+      .add("hn", hn)
+      .toString();
+  }
+}
