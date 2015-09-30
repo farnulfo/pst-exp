4eb9298f7f2857dc4fe961c12a53222548a2a240
Franck Arnulfo <franck.arnulfo@gmail.com>
2015-09-30 22:05:56 +0200

    Add Special Internal NIDs

---
+++ src/main/java/org/pipoware/pst/exp/SpecialInternalNID.java
@@ -0,0 +1,24 @@
+package org.pipoware.pst.exp;
+
+/**
+ *
+ * @author Franck Arnulfo
+ */
+public class SpecialInternalNID {
+
+  public static final int NID_MESSAGE_STORE = 0x21;
+  public static final int NID_NAME_TO_ID_MAP = 0x61;
+  public static final int NID_NORMAL_FOLDER_TEMPLATE = 0xA1;
+  public static final int NID_SEARCH_FOLDER_TEMPLATE = 0xC1;
+  public static final int NID_ROOT_FOLDER = 0x122;
+  public static final int NID_SEARCH_MANAGEMENT_QUEUE = 0x1E1;
+  public static final int NID_SEARCH_ACTIVITY_LIST = 0x201;
+  public static final int NID_RESERVED1 = 0x241;
+  public static final int NID_SEARCH_DOMAIN_OBJECT = 0x261;
+  public static final int NID_SEARCH_GATHERER_QUEUE = 0x281;
+  public static final int NID_SEARCH_GATHERER_DESCRIPTOR = 0x2A1;
+  public static final int NID_RESERVED2 = 0x2E1;
+  public static final int NID_RESERVED3 = 0x301;
+  public static final int NID_SEARCH_GATHERER_FOLDER_QUEUE = 0x321;
+
+}
