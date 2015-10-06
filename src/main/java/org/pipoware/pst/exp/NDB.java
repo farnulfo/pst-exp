f6992a5109e1c5d34e8fe7483de2288446a46a47
Franck Arnulfo <franck.arnulfo@gmail.com>
2015-10-06 09:10:53 +0200

    Add getBlockIdFromNID, getBLockFromNID and getPCFromNID

--- src/main/java/org/pipoware/pst/exp/NDB.java
+++ src/main/java/org/pipoware/pst/exp/NDB.java
@@ -32,4 +32,65 @@ public class NDB {
     return new Page(bytes, pst.getHeader().getType());
   }
 
+  public long getBlockIdFromNID(int nid) throws IOException {
+    Page rootNBTPage = fetchPage(header.getRoot().bRefNBT.getIb());
+    return getBlockIdFromNID(rootNBTPage, nid);
+  }
+  
+  private long getBlockIdFromNID(Page page, int nid) throws IOException {
+    if (page.getDepthLevel() == 0) {
+      for (NBTENTRY nbtentry : page.nbtentries) {
+        if (nbtentry.nid.data == nid) {
+          return nbtentry.bidData;
+        }
+      }
+      return -1;
+    } else {
+      for (BTENTRY btentry : page.btentries) {
+        pst.position(btentry.bref.getIb());
+        byte[] b = new byte[Page.PAGE_SIZE];
+        pst.read(b);
+        Page p = new Page(b, pst.getHeader().getType());
+        long bid = getBlockIdFromNID(p, nid);
+        if (bid != -1) {
+          return bid;
+        }
+      }
+    }
+    throw new IllegalArgumentException("NID " + nid +" no found.");
+  }
+  
+      
+  public PC getPCFromNID(int nid) throws IOException {
+    long bid = getBlockIdFromNID(nid);
+    Page page = fetchPage(pst.getHeader().getRoot().bRefBBT.getIb());
+    Block block = getBlockFromBID(page, bid);
+    HN hn = new HN(block.data);
+    BTH bth = new BTH(hn);
+    PC pc = new PC(bth);
+    return pc;
+  }
+
+  private Block getBlockFromBID(Page page, long bid) throws IOException {
+    if (page.getDepthLevel() == 0) {
+      for (BBTENTRY bbentry : page.bbtentries) {
+        if (bbentry.bref.getBid() == bid) {
+          Block block = new Block(pst, bbentry, pst.getHeader().getType());
+          return block;
+        }
+      }
+    } else {
+      for (BTENTRY btentry : page.btentries) {
+        pst.position(btentry.bref.getIb());
+        byte[] b = new byte[Page.PAGE_SIZE];
+        pst.read(b);
+        Page p = new Page(b, pst.getHeader().getType());
+        Block block = getBlockFromBID(p, bid);
+        if (block != null) {
+          return block;
+        }
+      }
+    }
+    return null;
+  }
 }
