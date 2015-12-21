057cd4796c25c2a7503bfa59820a1bb583c72926
Franck Arnulfo <franck.arnulfo@gmail.com>
2015-12-21 18:01:24 +0100

    Add EntryID class

---
+++ src/main/java/org/pipoware/pst/exp/EntryID.java
@@ -0,0 +1,35 @@
+package org.pipoware.pst.exp;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.io.BaseEncoding;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.charset.StandardCharsets;
+
+/**
+ *
+ * @author Franck Arnulfo
+ */
+public class EntryID {
+  
+  public final int rgbFlags;
+  public byte[] uid;
+  public int nid;
+  
+  public EntryID(byte[] bytes) {
+    ByteBuffer bb = ByteBuffer.wrap(bytes).order(ByteOrder.LITTLE_ENDIAN);
+    rgbFlags = bb.getInt();
+    uid = new byte[16];
+    bb.get(uid);
+    nid = bb.getInt();
+  }
+  
+  @Override
+  public String toString() {
+    return MoreObjects.toStringHelper(this)
+      .add("rgbFlags", Integer.toHexString(rgbFlags))
+      .add("uid", "[" + BaseEncoding.base16().withSeparator(",", 2).encode(uid) + "]")
+      .add("nid", Integer.toHexString(nid))
+      .toString();
+  }
+}
