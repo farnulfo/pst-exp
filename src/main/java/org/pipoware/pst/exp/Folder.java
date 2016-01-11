7c75d849ce2db32a3ab9e4a814c76af534bb03e1
Franck Arnulfo <franck.arnulfo@gmail.com>
2016-01-11 23:19:03 +0100

    Implement Message Iterator on Folder

--- src/main/java/org/pipoware/pst/exp/Folder.java
+++ src/main/java/org/pipoware/pst/exp/Folder.java
@@ -3,7 +3,11 @@ package org.pipoware.pst.exp;
 import com.google.common.base.MoreObjects;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import static org.pipoware.pst.exp.PropertyIdentifier.PidTagContentCount;
 import static org.pipoware.pst.exp.PropertyIdentifier.PidTagContentUnreadCount;
 import static org.pipoware.pst.exp.PropertyIdentifier.PidTagDisplayName;
@@ -13,7 +17,7 @@ import static org.pipoware.pst.exp.PropertyIdentifier.PidTagSubfolders;
  *
  * @author Franck Arnulfo
  */
-public class Folder {
+public class Folder implements Iterable<Message> {
 
   private final NDB ndb;
   private final PC folderPC;
@@ -89,6 +93,14 @@ public class Folder {
     }
     return messages;
   }
+  
+  public Iterator<Message> iterator() {
+    return new MessageIterator();
+  }
+  
+  public Iterator<Message> messages() {
+    return new MessageIterator();
+  }
 
   @Override
   public String toString() {
@@ -98,5 +110,39 @@ public class Folder {
             .add("contentUnreadCount", contentUnreadCount)
             .add("hasSubFolders", subFolders)
             .toString();
+  }
+
+  private class MessageIterator implements Iterator<Message> {
+
+    private int cursor;
+
+    public MessageIterator() {
+      this.cursor = 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+      return cursor < Folder.this.contentTable.getRows().size();
+    }
+
+    @Override
+    public Message next() {
+      if (this.hasNext()) {
+        int rowId = Folder.this.contentTable.getRows().get(cursor).dwRowID;
+        try {
+          Message message = new Message(ndb, rowId);
+          cursor++;
+          return message;
+        } catch (IOException ex) {
+          Logger.getLogger(Folder.class.getName()).log(Level.SEVERE, null, ex);
+        }
+      }
+      throw new NoSuchElementException();
+    }
+
+    @Override
+    public void remove() {
+      throw new UnsupportedOperationException();
+    }
   }
 }
