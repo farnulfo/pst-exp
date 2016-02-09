387af0990e1a005fc7aa7cbb1a8ea7a818895e38
Franck Arnulfo <franck.arnulfo@gmail.com>
2016-02-10 00:10:59 +0100

    Add some PropertyIdentifier

--- src/main/java/org/pipoware/pst/exp/PropertyIdentifier.java
+++ src/main/java/org/pipoware/pst/exp/PropertyIdentifier.java
@@ -5,11 +5,15 @@ package org.pipoware.pst.exp;
  * @author Franck Arnulfo
  */
 public class PropertyIdentifier {
+
   
   private PropertyIdentifier() {
   }
 
+  public static final short PidTagMessageDeliveryTime = (short) 0x0E06;
   public static final short PidTagDisplayName = (short) 0x3001;
+  public static final short PidTagCreationTime = (short) 0x3007;
+  public static final short PidTagLastModificationTime = (short) 0x3008;
   public static final short PidTagContentCount = (short) 0x3602;
   public static final short PidTagContentUnreadCount = (short) 0x3603;
   public static final short PidTagSubfolders = (short) 0x360A;
