35f6988b86ed90dee5f837b97266880264b845a5
Franck Arnulfo <franck.arnulfo@gmail.com>
2017-01-05 23:13:20 +0100

    Add MessageTest.java

---
+++ src/test/java/org/pipoware/pst/exp/MessageTest.java
@@ -0,0 +1,49 @@
+package org.pipoware.pst.exp;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author Franck Arnulfo
+ */
+public class MessageTest {
+
+  @Test
+  public void testMessageProperty() throws IOException, URISyntaxException {
+    PSTFile pstFile = new PSTFile(Paths.get(getClass().getResource("/pstsdk/sample1.pst").toURI()));
+    NDB ndb = new NDB(pstFile, pstFile.getHeader());
+    int messageNid = 0x200024;
+    PC messagePC = ndb.getPCFromNID(messageNid);
+    System.out.println("PC:");
+    for (PCItem item : messagePC.items) {
+      System.out.println(item);
+    }
+  }
+  
+  @Test
+  public void testMessagePropertySubject1() throws IOException, URISyntaxException {
+    PSTFile pstFile = new PSTFile(Paths.get(getClass().getResource("/pstsdk/sample1.pst").toURI()));
+    NDB ndb = new NDB(pstFile, pstFile.getHeader());
+    int messageNid = 0x200024;
+    PC messagePC = ndb.getPCFromNID(messageNid);
+    Message message = new Message(ndb, messageNid);
+    String subject = message.getSubject();
+    Assert.assertEquals("Here is a sample message", message.getSubject());
+  }
+  
+  @Test
+  public void testMessagePropertySubject2() throws IOException, URISyntaxException {
+    PSTFile pstFile = new PSTFile(Paths.get(getClass().getResource("/pstsdk/submessage.pst").toURI()));
+    NDB ndb = new NDB(pstFile, pstFile.getHeader());
+    int messageNid = 0x200024;
+    PC messagePC = ndb.getPCFromNID(messageNid);
+    Message message = new Message(ndb, messageNid);
+    String subject = message.getSubject();
+    Assert.assertEquals("This is a message which has an embedded message attached", message.getSubject());
+  }
+
+}
